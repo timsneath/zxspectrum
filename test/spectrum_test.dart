d31b94c1f57dcb13be3448ba55a60753e92c7e9d
Tim Sneath <tim@sneath.org>
2022-08-17 20:46:34 -0700

    Add CI/CD

--- test/spectrum_test.dart
+++ test/spectrum_test.dart
@@ -13,4 +13,20 @@ void main() {
     final resetMemory = spectrum.memory.read(0x4000, 0x10000 - 0x4000);
     expect(resetMemory, everyElement(0));
   });
+
+  test('Boot is successful', () {
+    const breakpoint = 0x15e6;
+    final rom = File('roms/48.rom').readAsBytesSync();
+    final spectrum = Spectrum(rom);
+    while (spectrum.z80.pc != breakpoint) {
+      spectrum.z80.executeNextInstruction();
+    }
+    expect(spectrum.z80.af, equals(0x0018));
+    expect(spectrum.z80.bc, equals(0x174b));
+    expect(spectrum.z80.de, equals(0x0006));
+    expect(spectrum.z80.hl, equals(0x107f));
+    expect(spectrum.z80.ix, equals(0xffff));
+    expect(spectrum.z80.iy, equals(0x5c3a));
+    expect(spectrum.z80.sp, equals(0xff4c));
+  });
 }
